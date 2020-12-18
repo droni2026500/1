a59a5c0710ac22e08a7beca96c3df0282ac660bd
droni2026500 <>
2020-12-18 11:06:38 +0300

    third committ

--- src/api/getVacancy.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const getVacancy = (text) => axios.get(`http://opendata.trudvsem.ru/api/v1/vacancies?text=${text}&region_code=7700000000000`);
-
-export default getVacancy;
--- src/components/Search.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Search = () => (
-  <div>
-    dfdf
-  </div>
-);
-
-export default Search;
